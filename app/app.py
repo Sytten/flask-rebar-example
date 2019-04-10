2456dd643d0c5319c29b45ce5cb7713be0706f85
Emile Fugulin <emilefugulin@hotmail.com>
2019-04-09 22:55:07 -0400

    Add database connection

--- app/app.py
+++ app/app.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_rebar import Rebar
+from flask_sqlalchemy import SQLAlchemy
 
 
 rebar = Rebar()
 registry = rebar.create_handler_registry(prefix='/api')
+db = SQLAlchemy()
+migrate = Migrate(db=db)
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
+    app.config.from_envvar("APP_CONFIG")
+
     rebar.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app)
     return app
-
-
-if __name__ == '__main__':
-    create_app().run()
